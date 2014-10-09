a6a01e6c4f84150b63967204d2b17c4947880f32
timbits07 <tim.storey@yahoo.com>
2014-10-09 13:04:32 -0700

    fixed default picture image

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ActiveRecord::Base
 	belongs_to :user
 	validates :title, presence: true
 	validates :body, presence: true
-	has_attached_file :picture, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+	has_attached_file :picture, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => ""
   validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
 end
